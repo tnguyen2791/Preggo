4f32376d857b77574a2a7288078bd12eac993c4b
tnguyen2791 <tnguyen2791@gmail.com>
2022-04-25 14:40:28 -0400

    Creation of menu and the homepage.

--- lib/home.dart
+++ lib/home.dart
@@ -101,80 +101,83 @@ class Homepage extends StatelessWidget {
           Expanded(
             flex: 2,
             child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ReusableCard(
-                    colour: Colors.grey,
-                    onPress: () {
-                      print('Hello');
-                    },
-                    cardDescription: Text(
-                      'Weigh in!',
-                      style: kMenuTextStyle,
-                    ),
-                    cardIcon: Icon(
-                      FontAwesomeIcons.weightScale,
-                      size: 40.0,
-                    ),
-                  ),
-                  ReusableCard(
-                    colour: Colors.grey,
-                    onPress: () {
-                      print('Hello');
-                    },
-                    cardDescription: Text(
-                      'Are you hydrated?',
-                      style: kMenuTextStyle,
-                    ),
-                    cardIcon: Icon(
-                      FontAwesomeIcons.droplet,
-                      size: 40.0,
-                    ),
-                  ),
-                  ReusableCard(
-                    colour: Colors.grey,
-                    onPress: () {
-                      print('Hello');
-                    },
-                    cardDescription: Text(
-                      'Exercise Regimen',
-                      style: kMenuTextStyle,
-                    ),
-                    cardIcon: Icon(
-                      FontAwesomeIcons.dumbbell,
-                      size: 40.0,
-                    ),
-                  ),
-                  ReusableCard(
-                    colour: Colors.grey,
-                    onPress: () {
-                      print('Hello');
-                    },
-                    cardDescription: Text(
-                      'Diet',
-                      style: kMenuTextStyle,
-                    ),
-                    cardIcon: Icon(
-                      FontAwesomeIcons.carrot,
-                      size: 40.0,
-                    ),
-                  ),
-                  ReusableCard(
-                    colour: Colors.grey,
-                    onPress: () {
-                      print('Hello');
-                    },
-                    cardDescription: Text(
-                      'Resources',
-                      style: kMenuTextStyle,
-                    ),
-                    cardIcon: Icon(
-                      FontAwesomeIcons.boxArchive,
-                      size: 40.0,
-                    ),
-                  ),
-                ],
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ReusableCard(
+                      colour: Colors.grey,
+                      onPress: () {
+                        print('Hello');
+                      },
+                      cardDescription: const Text(
+                        'Weigh in!',
+                        style: kMenuTextStyle,
+                      ),
+                      cardIcon: const Icon(
+                        FontAwesomeIcons.weightScale,
+                        size: 40.0,
+                      ),
+                    ), //Weighing in
+                    ReusableCard(
+                      colour: Colors.grey,
+                      onPress: () {
+                        print('Hello');
+                      },
+                      cardDescription: const Text(
+                        'Are you hydrated?',
+                        style: kMenuTextStyle,
+                      ),
+                      cardIcon: const Icon(
+                        FontAwesomeIcons.droplet,
+                        size: 40.0,
+                      ),
+                    ), //Hydration
+                    ReusableCard(
+                      colour: Colors.grey,
+                      onPress: () {
+                        print('Hello');
+                      },
+                      cardDescription: const Text(
+                        'Exercise Regimen',
+                        style: kMenuTextStyle,
+                      ),
+                      cardIcon: const Icon(
+                        FontAwesomeIcons.dumbbell,
+                        size: 40.0,
+                      ),
+                    ), //Exercise Regimen
+                    ReusableCard(
+                      colour: Colors.grey,
+                      onPress: () {
+                        print('Hello');
+                      },
+                      cardDescription: const Text(
+                        'Diet',
+                        style: kMenuTextStyle,
+                      ),
+                      cardIcon: const Icon(
+                        FontAwesomeIcons.carrot,
+                        size: 40.0,
+                      ),
+                    ), //Diet
+                    ReusableCard(
+                      colour: Colors.grey,
+                      onPress: () {
+                        print('Hello');
+                      },
+                      cardDescription: const Text(
+                        'Resources',
+                        style: kMenuTextStyle,
+                      ),
+                      cardIcon: const Icon(
+                        FontAwesomeIcons.boxArchive,
+                        size: 40.0,
+                      ),
+                    ), //Resources
+                  ],
+                ),
               ),
             ),
           )
